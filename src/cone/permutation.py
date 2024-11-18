0020ceab5126c5ab8487ba36ec0483c4fe8aa772
Roland DENIS <denis@math.univ-lyon1.fr>
2024-11-18 20:25:38 +0100

    Adding Permutation class with length, inverse and generator
    Generation of all the permutation only (currently)

---
+++ src/cone/permutation.py
@@ -0,0 +1,45 @@
+from .typing import *
+from functools import cached_property
+import itertools
+
+class Permutation(tuple[int, ...]):
+    """
+    Permutation represented using the one-line notation.
+
+    So that length computation is faster.
+    """
+
+    @cached_property
+    def length(self) -> int:
+        """ Length of the permutation, ie number of inversions """
+        return sum(
+            1 if self[i] > self[j] else 0
+            for i, j in itertools.combinations(range(len(self)), 2)
+        )
+    
+    @cached_property
+    def inverse(self) -> "Permutation":
+        """ Inverse of the permutation """
+        inv = [0] * len(self)
+        for i, pi in enumerate(self):
+            inv[pi] = i
+        return Permutation(inv)
+
+    def __call__(self, s: Sequence[T]) -> tuple[T, ...]:
+        """ Apply the permutation to a given sequence """
+        return tuple(s[pi] for pi in self)
+    
+    def __repr__(self) -> str:
+        return f"Permutation({super().__repr__()})"
+    
+    @staticmethod
+    def all(n: int) -> Iterable["Permutation"]:
+        """ Returns all the possible permutation of S_n """
+        for p in itertools.permutations(range(n)):
+            yield Permutation(p)
+
+    @staticmethod
+    def all_of_length(n: int, l: int) -> Iterable["Permutation"]:
+        """ Returns all permutations of S_n with given length l """
+        return NotImplemented
+    
---
+++ tests/test_permutation.py
@@ -0,0 +1,21 @@
+import unittest
+
+from cone.permutation import Permutation
+
+class TestPermutation(unittest.TestCase):
+    def test_interface(self) -> None:
+        # (1,20,16,12,8,4,21,17,13,9,5)(2,18,14,10,6)(3,19,15,11,7)
+        # with cycle length 11, 5, 5
+        wp = (19, 17, 18, 20, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+        p = Permutation(wp)
+        self.assertEqual(len(p), 21)
+        self.assertEqual(p.length, 70)
+        self.assertEqual(p(range(21)), wp)
+        self.assertEqual(p(p(range(21))), (15, 13, 14, 16, 19, 17, 18, 20, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12))
+
+        s = range(21)
+        for _ in range(55): # lcm(11, 5, 5) = 55
+            s = p(s)
+        self.assertEqual(s, tuple(range(21)))
+
+        self.assertEqual(p.inverse(p(range(21))), tuple(range(21)))
