d7c080cac33b1ca4c3b1e63b0dde15f609b4847a
Roland DENIS <denis@math.univ-lyon1.fr>
2024-11-18 20:26:12 +0100

    Unit test for Root class

---
+++ tests/test_root.py
@@ -0,0 +1,26 @@
+import unittest
+
+from cone.root import Root
+from cone.dimension import Dimension
+
+class TestRoot(unittest.TestCase):
+    def test_interface(self) -> None:
+        r1 = Root(1, 2, 3)
+        r2 = Root(1, 3, 2)
+
+        self.assertEqual(r1.k, 1)
+        self.assertEqual(r1.i, 2)
+        self.assertEqual(r1.j, 3)
+
+        self.assertTrue(r1.is_in_U)
+        self.assertFalse(r2.is_in_U)
+
+    def test_all(self) -> None:
+        d = Dimension((2, 3, 4))
+        all_r = tuple(Root.all(d))
+
+        self.assertEqual(len(all_r), 10)
+        self.assertEqual(all_r[0], Root(0, 0, 1))
+        self.assertEqual(all_r[3], Root(1, 1, 2))
+        self.assertEqual(all_r[5], Root(2, 0, 2))
+        self.assertEqual(all_r[9], Root(2, 2, 3))
