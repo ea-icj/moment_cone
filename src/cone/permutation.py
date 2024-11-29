8f0507b5e5cc9eb6a15e2d53eb24f58524dc7da3
Roland DENIS <denis@math.univ-lyon1.fr>
2024-11-29 11:18:29 +0100

    Some more (static) methods on Permutation (maybe useless)

--- src/cone/permutation.py
+++ src/cone/permutation.py
@@ -16,6 +16,28 @@ class Permutation(tuple[int, ...]): # Remark: hash of p is hash of underlying tu
 
     The coefficients are the image of range(n) by the permutation.
     So that length computation is faster.
+
+    Example:
+    >>> from cone.permutation import Permutation
+    >>> p = Permutation((3, 2, 1, 4, 0, 5))
+    >>> p
+    Permutation((3, 2, 1, 4, 0, 5))
+    >>> p.n
+    6
+    >>> p.inverse
+    Permutation((4, 2, 1, 0, 3, 5))
+    >>> p(p.inverse)
+    (0, 1, 2, 3, 4, 5)
+    >>> p.length
+    7
+    >>> for p in Permutation.all(3):
+    ...     print(p)
+    Permutation((0, 1, 2))
+    Permutation((0, 2, 1))
+    Permutation((1, 0, 2))
+    Permutation((1, 2, 0))
+    Permutation((2, 0, 1))
+    Permutation((2, 1, 0))
     """
     @property
     def n(self) -> int:
@@ -52,7 +74,7 @@ class Permutation(tuple[int, ...]): # Remark: hash of p is hash of underlying tu
         return f"Permutation({super().__repr__()})"
     
     def is_min_rep(self, symmetries: Iterable[int]) -> bool:
-        """ Check is permutation is decreasing along each block of given sizes """
+        """ Check if permutation is decreasing along each block of given sizes """
         return all(
             all(a > b for a, b in itertools.pairwise(block))
             for block in Blocks.from_flatten(self, symmetries)
@@ -71,7 +93,6 @@ class Permutation(tuple[int, ...]): # Remark: hash of p is hash of underlying tu
         for p in itertools.product(*blocks):
             yield Permutation(itertools.chain.from_iterable(p))
 
-
     @staticmethod
     def all(n: int) -> Iterable["Permutation"]:
         """ Returns all the possible permutation of S_n """
@@ -88,6 +109,48 @@ class Permutation(tuple[int, ...]): # Remark: hash of p is hash of underlying tu
         # More efficient way ?
         return filter(lambda p: p.length == l, Permutation.all(n))
     
+    @staticmethod
+    def from_cycles(n: int, *cycles: Sequence[int]) -> "Permutation":
+        """
+        Returns a permutation from a list of cycles
+        
+        >>> Permutation.from_cycles(5, (2, 1), (3, 0, 4))
+        Permutation((4, 2, 1, 0, 3))
+        """
+        p = list(range(n))
+        for cycle in cycles:
+            for i, j in itertools.pairwise(cycle):
+                p[i] = j
+            p[cycle[-1]] = cycle[0]
+        return Permutation(p)
+    
+    def transpose(self, i: int, j: int) -> "Permutation":
+        """
+        Apply a transposition between ith and jth positions
+
+        >>> p = Permutation((4, 2, 1, 0, 3))
+        >>> p.transpose(3, 1)
+        Permutation((4, 0, 1, 2, 3))
+        """
+        if i > j:
+            i, j = j, i
+        return Permutation(
+            self[:i] + self[j:j+1] + self[i+1:j] + self[i:i+1] + self[j+1:]
+        )
+
+    @staticmethod
+    def all_transpositions(n: int) -> Iterable["Permutation"]:
+        """
+        Returns all transpositions
+        
+        >>> for p in Permutation.all_transpositions(3):
+        ...     print(p)
+        Permutation((1, 0, 2))
+        Permutation((2, 1, 0))
+        Permutation((0, 2, 1))
+        """
+        return (Permutation.from_cycles(n, c) for c in itertools.combinations(range(n), 2))
+
 
 class AllPermutationsByLength:
     """
@@ -96,6 +159,17 @@ class AllPermutationsByLength:
     Since Permutation use a cache for its properties, that means
     this catalogue can use lot of memory to store each permutation,
     it's list of inversion, it's length, ...
+
+    Examples:
+    >>> ap = AllPermutationsByLength(3)
+    >>> len(ap) # Number of different lengths
+    4
+    >>> ap.max_length # Maximal length (should be len(ap) - 1)
+    3
+    >>> ap[1] # Permutations of length 1
+    (Permutation((0, 2, 1)), Permutation((1, 0, 2)))
+    >>> ap[:2] # All permutations of length <= 1
+    (Permutation((0, 1, 2)), Permutation((0, 2, 1)), Permutation((1, 0, 2)))
     """
     __slots__ = 'permutations', 'indexes'
     permutations: tuple[Permutation, ...]
