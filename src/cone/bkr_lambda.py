ab5c3a4df19f93c47a4353c5bf08921d369f4b48
Roland DENIS <denis@math.univ-lyon1.fr>
2025-02-09 19:30:36 +0100

    Fixing corner cases of Kronecker product & coeff

--- src/cone/bkr_lambda.py
+++ src/cone/bkr_lambda.py
@@ -13,23 +13,54 @@ def all_partitions_of_max_length(n: int, l: Sequence[int], kro: KroneckerCoeffic
     """
     All len(l)-uplets of partitions of n of non-zero Kronecker coefficient
     and so that len(p_i) <= l_i
+
+    Examples:
+    >>> from cone.kronecker import KroneckerCoefficientMLCache
+    >>> kc = KroneckerCoefficientMLCache()
+    >>> for p in all_partitions_of_max_length(3, [2, 3, 2], kc):
+    ...    print(p)
+    ((Partition((3,)), Partition((3,)), Partition((3,))), 1)
+    ((Partition((3,)), Partition((2, 1)), Partition((2, 1))), 1)
+    ((Partition((2, 1)), Partition((2, 1)), Partition((3,))), 1)
+    ((Partition((2, 1)), Partition((1, 1, 1)), Partition((2, 1))), 1)
+    ((Partition((2, 1)), Partition((2, 1)), Partition((2, 1))), 1)
+    ((Partition((2, 1)), Partition((3,)), Partition((2, 1))), 1)
+
+    >>> for p in all_partitions_of_max_length(3, [2, 2], kc):
+    ...    print(p)
+    ((Partition((3,)), Partition((3,))), 1)
+    ((Partition((2, 1)), Partition((2, 1))), 1)
+    >>> list(all_partitions_of_max_length(3, [2, 0, 2], kc))
+    []
+    >>> list(all_partitions_of_max_length(3, [2], kc))
+    [((Partition((3,)),), 1)]
+    >>> list(all_partitions_of_max_length(3, [0], kc))
+    []
+    >>> list(all_partitions_of_max_length(3, [], kc))
+    []
     """
-    # Sort by increasing maximal length (faster) and keep order
-    permutation, sorted_l = zip(*sorted(enumerate(l), key=lambda l: l[1]))
-    p_inverse = Permutation(permutation).inverse
+    if len(l) == 1 and l[0] >= 1:
+        yield (Partition(n),), 1
+    elif len(l) == 2:
+        for p in Partition.all_for_integer(n, max_length=min(l)):
+            yield (p, p), 1
+    elif len(l) >= 3:
+        # Sort by increasing maximal length (faster) and keep order
+        permutation, sorted_l = zip(*sorted(enumerate(l), key=lambda l: l[1]))
+        p_inverse = Permutation(permutation).inverse
 
-    # All nuplets of partitions without the last length constraint
-    head_product = itertools.product(
-        *(Partition.all_for_integer(n, li) for li in sorted_l[:-1])
-    )
+        # All nuplets of partitions without the last length constraint
+        head_product = itertools.product(
+            *(Partition.all_for_integer(n, li) for li in sorted_l[:-1])
+        )
 
-    # Computing the product and yielding only the partitions of the decomposition
-    # whose length respects the last constraint.
-    for head in head_product:
-        product = kro.product(head)
-        for p, c in product.items():
-            if len(p) <= sorted_l[-1]:
-                yield p_inverse(head + (p,)), c
+        # Computing the product and yielding only the partitions of the decomposition
+        # whose length respects the last constraint.
+        for head in head_product:
+            product = kro.product(head)
+            for p, c in product.items():
+                if len(p) <= sorted_l[-1]:
+                    yield p_inverse(head + (p,)), c
 
 
 def all_lambda_matrix(delta: Sequence[int], max_length: NDArray, kro: KroneckerCoefficient) -> Iterable[tuple[NDArray, int]]:
